f6174a064c0aeb6781569afba55ff282ce7ca137
Anthony Wikkerink <Anthony.Wikkerink@D2L.com>
2018-10-31 16:29:33 -0400

    Polymer 3 Conversion

---
+++ d2l-fetch.js
@@ -0,0 +1,7 @@
+/* CHANGES TO THIS FILE WILL BE LOST - IT IS AUTOMATICALLY GENERATED WHEN d2l-fetch IS RELEASED */
+/*
+  FIXME(polymer-modulizer): the above comments were extracted
+  from HTML and may be out of place here. Review them and
+  then delete this comment!
+*/
+;
