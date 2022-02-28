153d98616f618e58144e50ef7662b9f053bb02b0
Ryan Mahler <Ryan.Mahler@D2L.com>
2022-02-28 11:35:37 -0800

    Update dependencies
    No real changes here aside from whitespace and ordering, as the new
    version of eslint-config-brightspace also includes `sort-class-members`.
    
    Closes #49
    Closes #50
    Closes #51
    Closes #52
    Closes #55
    Closes #56
    Closes #61
    Closes #62
    Closes #63

--- src/d2lfetch.js
+++ src/d2lfetch.js
@@ -4,26 +4,8 @@ export class D2LFetch {
 		this._installedMiddlewares = [];
 	}
 
-	fetch(input, options) {
-		if ('string' === typeof input) {
-			input = new Request(input, options);
-		}
-		if (false === input instanceof Request) {
-			return Promise.reject(new TypeError('Invalid input argument(s) supplied.'));
-		}
-
-		const chain = this._installedMiddlewares.slice();
-		chain.push({ name: 'fetch', fn: this._wrapMiddleware(window.fetch) });
-		return chain.shift().fn.bind(this, chain)(input);
-	}
-
-	use(middleware) {
-		const {name, fn, options} = this._verifyMiddleware(middleware);
-		this._installedMiddlewares.push({ name, fn: this._wrapMiddleware(fn, options) });
-	}
-
-	addTemp(middleware, { prepend = false } = { prepend: false } ) {
-		const {name, fn, options} = this._verifyMiddleware(middleware);
+	addTemp(middleware, { prepend = false } = { prepend: false }) {
+		const { name, fn, options } = this._verifyMiddleware(middleware);
 		const self = new D2LFetch();
 
 		self._installedMiddlewares = this._installedMiddlewares.slice();
@@ -37,6 +19,19 @@ export class D2LFetch {
 		return self;
 	}
 
+	fetch(input, options) {
+		if ('string' === typeof input) {
+			input = new Request(input, options);
+		}
+		if (false === input instanceof Request) {
+			return Promise.reject(new TypeError('Invalid input argument(s) supplied.'));
+		}
+
+		const chain = this._installedMiddlewares.slice();
+		chain.push({ name: 'fetch', fn: this._wrapMiddleware(window.fetch) });
+		return chain.shift().fn.bind(this, chain)(input);
+	}
+
 	removeTemp(name) {
 		if (!name || typeof name !== 'string') {
 			throw TypeError('Middleware name must be a non-empty string');
@@ -44,6 +39,25 @@ export class D2LFetch {
 		const self = new D2LFetch();
 		self._installedMiddlewares = this._installedMiddlewares.filter(middleware => middleware.name !== name);
 		return self;
+	}
+
+	use(middleware) {
+		const { name, fn, options } = this._verifyMiddleware(middleware);
+		this._installedMiddlewares.push({ name, fn: this._wrapMiddleware(fn, options) });
+	}
+
+	_verifyMiddleware(middleware) {
+		if (middleware && typeof middleware === 'object') {
+			const { name, fn } = middleware;
+			if (name && typeof name === 'string' &&
+				fn && typeof fn === 'function') {
+				return middleware;
+			} else {
+				throw TypeError('Middleware name/function is undefined or not a string/function');
+			}
+		} else {
+			throw TypeError('Middleware parameter is undefined/null/empty or not an object');
+		}
 	}
 
 	_wrapMiddleware(fn, options) {
@@ -54,19 +68,5 @@ export class D2LFetch {
 			}
 			return fn(request, next, options);
 		};
-	}
-
-	_verifyMiddleware(middleware) {
-		if (middleware && typeof middleware === 'object') {
-			const {name, fn} = middleware;
-			if (name && typeof name === 'string' &&
-				fn && typeof fn === 'function') {
-				return middleware;
-			} else {
-				throw TypeError('Middleware name/function is undefined or not a string/function');
-			}
-		} else {
-			throw TypeError('Middleware parameter is undefined/null/empty or not an object');
-		}
 	}
 }
